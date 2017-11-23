97f7da9bb7f0cb5221e21d22a67bb24b33768472
rmolines <rafaelmismael@gmail.com>
2017-11-22 23:06:55 -0200

    ULA E UCULA PRONTAS

---
+++ UCULA.vhd
@@ -0,0 +1,46 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use ieee.numeric_std.all;
+
+entity UCULA is
+  port (
+  clk	  : in std_logic;
+  OP    : in std_logic_vector(1 downto 0);
+  funct : in std_logic_vector(5 downto 0);
+  ctrl  : out std_logic_vector(3 downto 0)
+  );
+end entity;
+
+architecture UCULAarch of UCULA  is
+
+
+begin
+
+  process (OP, funct, clk) is
+    variable tempCtrl : std_logic_vector (3 downto 0);
+    variable tempFunct : std_logic_vector (5 downto 0);
+  begin
+	tempFunct := funct;
+  	if(rising_edge(clk)) then
+      case OP is
+        when "00" => tempCtrl := "0010";
+        when "01" => tempCtrl := "0110";
+        when "10" =>
+          case tempFunct(3 downto 0) is
+            when "0000" => tempCtrl := "0010";
+            when "0010" => tempCtrl := "0110";
+            when "0100" => tempCtrl := "0000";
+            when "0101" => tempCtrl := "0001";
+            when "1010" => tempCtrl := "0111";
+            when others => tempCtrl := "1111";
+          end case;
+		    when others => tempCtrl := "1111";
+      end case;
+
+
+
+      ctrl <= tempCtrl;
+
+    end if;
+  end process;
+end architecture;
--- ULA.vhd
+++ ULA.vhd
@@ -1,38 +1,65 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity ULA is
 
-  Port ( A :  in std_logic_vector(31 downto 0);
-         B :  in std_logic_vector(31 downto 0);
+  Port ( A :  in std_logic_vector(31 downto 0) := std_logic_vector(signed(to_unsigned(30, 32)));
+         B :  in std_logic_vector(31 downto 0) := std_logic_vector(signed(to_unsigned(25, 32)));
          Sel: in std_logic_vector(3 downto 0);
          C : out std_logic_vector(31 downto 0);
-      	 overflow: out std_logic,
-			   zeroFlag : out std_logic
+			zeroFlag : out std_logic;
+			v  : out std_logic
  );
 end entity;
 
 architecture ULAarch of ULA is
 
-  signal muxAOut, muxBOut, adderOut
-         andOut, orOut: std_logic_vector(31 downto 0);
+  signal muxAOut, muxBOut, adderOut,
+         andOut, orOut, sltIn: std_logic_vector(31 downto 0);
+  signal overflow : std_logic;
 
 begin
 
-  C => muxResultOut;
-  zeroFlag => NOT muxResultOut;
-  
-  MuxA : entity work.MUX port map
-       (A => A, B => (NOT A), SEL => SEL(3),
-       C => muxAOut);
-
-  MuxB : entity work.MUX port map
-       (A => B, B => NOT B, SEL => SEL(2),
-       C => muxBOut);
-
-  Adder : entity work.adder port map
-       (A => muxAOut, B => muxBOut, C => adderOut,
-       );
-
-  MUXResult : entity work.MUX port map
-       (A => andOut, B => orOut, C => adderOut,
-       D => sltOut, E => muxResultOut);
-
+  process (A, B, Sel) is
+    variable tempOF : std_logic_vector(2 downto 0);
+    variable tempA  : std_logic_vector(31 downto 0);
+    variable tempB  : std_logic_vector(31 downto 0);
+    variable C9     : std_logic_vector(32 downto 0);
+    variable tempAdd: std_logic_vector(32 downto 0);
+  begin
+    tempA := (others => '0');
+    tempB := (others => '0');
+    case Sel(3) is
+      when '1' => tempA := std_logic_vector(to_signed(-to_integer(signed(A)), A'length));
+      when others => tempA := A;
+    end case;
+	 case sel(2) is
+      when '1' => tempB := std_logic_vector(to_signed(-to_integer(signed(B)), B'length));
+      when others => tempB := B;
+	 end case;
+	 tempAdd := std_logic_vector(resize(to_signed(to_integer(signed(tempA)) + to_integer(signed(tempB)), C'length), C9'length));
+    C9 := (others => '0');
+    case Sel(1 downto 0) is
+      when "11" => C9 := (0 => sltIn(0), others => '0');
+      when "10" => C9 := tempAdd;
+      when "01" => C9(32 downto 0) := '0' & std_logic_vector(tempA OR tempB);
+      when "00" => C9(32 downto 0) := '0' & std_logic_vector(tempA AND tempB);
+      when others => C9 := (others => '0');
+    end case;
+    C(31 downto 0) <= C9(31 downto 0);
+    -- overflow = "b001" (entrada POSITIVA e saida NEGATIVA)  OU "b110" (entrada NEGATIVA e saida POSITIVA)
+    tempOF := tempA(tempA'high) & tempB(tempB'high) & tempAdd(tempAdd'high-1);
+    case tempOF is
+      when "001"  => overflow <= '1';
+      when "110"  => overflow <= '1';
+      when others => overflow <= '0';
+    end case;
+	 v <= overflow;
+    sltIn <= (0 => overflow XOR tempAdd(31), others => '0');
+    case to_integer(signed(tempAdd(31 downto 0))) is
+      when 0 => zeroFlag <= '1';
+      when others => zeroFlag <= '0';
+    end case;
+  end process;
 end architecture;
